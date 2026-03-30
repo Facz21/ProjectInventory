aa846387b4a752d7254585103424083ca2b5f78e
Facz21 <afcz2001@gmail.com>
2026-03-30 18:53:34 -0500

    main menu view added

---
+++ ui/menus.py
@@ -0,0 +1,19 @@
+def show_main_menu():
+    """
+    This function show the main menu with number based options 
+    """
+    print("""
+  ==================       
+||     MAIN MENU    ||
+  ==================
+||1.Add product     ||
+||2.Show products   ||
+||3.Search product  ||
+||4.Update product  ||
+||5.Delete product  ||
+||6.Show stadistics ||
+||7.Save on a CSV   ||
+||8.Load on a CSV   ||
+||0.Exit            ||
+  =================          
+          """)
